d9a1ae30c53c44c39baa89f597f17cd159109fb5
Ross Voerman <voermanr@oregonstate.edu>
2022-10-03 11:43:05 -0700

    cleaned up the code

--- main.rb
+++ main.rb
@@ -3,33 +3,21 @@
 @name = "Johanna Jackson"
 
 # Mangle the name by reversing it or borgifying it
-def modified_name(choose)
+def modify_name(name, choose)
   # Split the name into first and last in an array
-  split_name = @name.split
-  #reversed_twice = split_name.reverse.reverse
-  #mangled_name = reversed_twice << "Mangled"
-  #return mangled_name * 2
+  split_name = name.split
+
   # Reverse the array of name components so the array is [last, first]
   reversed_name_components = split_name.reverse
-  # Rejoin the array of name components
-  new_name1 = reversed_name_components.join(' ')
-  # Store the mangled name
-  @name = new_name1
-  puts new_name1
+
   if choose
-    # Split the name into last and first in an array
-    split_name2 = new_name1.split
-    # Reverse the array of name components so the array is [last, first]
-    reversed_name_components2 = split_name2.reverse
     # Add borg to the end of the array
-    reversed_name_components2 << "Borg"
-    # Rejoin the array of name components
-    new_name2 = reversed_name_components2.join(' ')
-    puts new_name2
-    @name = new_name2
+    reversed_name_components << "Borg"
   end
-  return @name
+
+  # Rejoin the array of name components
+  reversed_name_components.join(' ')
 end
 
-puts "New name: #{modified_name(false)}"
-puts "New borg name: #{modified_name(true)}"
+puts "New name: #{modify_name(@name,false)}"
+puts "New borg name: #{modify_name(@name,true)}"
