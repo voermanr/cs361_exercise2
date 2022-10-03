b2cb4accbc5208e8e0adbe9062c255964392aef5
Ross Voerman <voermanr@oregonstate.edu>
2022-10-03 11:51:20 -0700

    added borgify func and cleaned more code

--- main.rb
+++ main.rb
@@ -3,21 +3,20 @@
 @name = "Johanna Jackson"
 
 # Mangle the name by reversing it or borgifying it
-def modify_name(name, choose)
+def modify_name(name)
   # Split the name into first and last in an array
   split_name = name.split
 
   # Reverse the array of name components so the array is [last, first]
   reversed_name_components = split_name.reverse
 
-  if choose
-    # Add borg to the end of the array
-    reversed_name_components << "Borg"
-  end
-
   # Rejoin the array of name components
   reversed_name_components.join(' ')
 end
 
-puts "New name: #{modify_name(@name,false)}"
-puts "New borg name: #{modify_name(@name,true)}"
+def borgify_name(name)
+  modify_name(name) + " Borg"
+end
+
+puts "New name: #{modify_name(@name)}"
+puts "New borg name: #{borgify_name(@name)}"
