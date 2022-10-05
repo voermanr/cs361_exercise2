f82e89ac89727e021e374b6c4f0511c33bdc9111
Ross Voerman <voermanr@oregonstate.edu>
2022-10-05 11:43:37 -0700

    cleaned up the code

--- main.rb
+++ main.rb
@@ -1,22 +1,30 @@
+# frozen_string_literal: true
+
 # Name Mangler
 
-@name = "Johanna Jackson"
+@name = 'Johanna Jackson'
 
-# Mangle the name by reversing it or borgifying it
-def modify_name(name)
+def reverse_and_borgify_name(name)
+  "#{reverse_name(name)} Borg"
+end
+
+def reverse_name(name)
   # Split the name into first and last in an array
   split_name = name.split
 
   # Reverse the array of name components so the array is [last, first]
   reversed_name_components = split_name.reverse
 
-  # Rejoin the array of name components
   reversed_name_components.join(' ')
 end
 
-def borgify_name(name)
-  modify_name(name) + " Borg"
+def modified_name(name, choose)
+  if choose
+    reverse_and_borgify_name name
+  else
+    reverse_name name
+  end
 end
 
-puts "New name: #{modify_name(@name)}"
-puts "New borg name: #{borgify_name(@name)}"
+puts "New name: #{modified_name(@name, false)}"
+puts "New borg name: #{modified_name(@name, true)}"
